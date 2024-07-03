38bc3d65990abb7e72e24005b3f78d19dc594a3d
rkg8841-professional <rkg8841@gmail.com>
2024-07-03 15:06:46 +0000

    inserted dummy test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,27 @@
 import pytest
+
+
+
+@pytest.fixture
+def app():
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    return app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
+
 #from app import fhwebsite as flask_app
 #import fhwebsite as flask_app
 
